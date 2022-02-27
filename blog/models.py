162f461f8d00c38dc940c2c105fc178af03e1ac9
juliusjli <juliusjli@tencent.com>
2022-02-27 08:35:46 +0800

    add some model

---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Category(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITERMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除')
+    )
+    name = models.CharField(max_length=50, verbose_name="名称")
+    status = models.PositiveBigIntegerField(default=STATUS_NORMAL, choices=STATUS_ITERMS, verbose_name="状态")
+    is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
+    owner = models.ForeignKey(User, verbose_name="作者")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = "分类"
+
+
+class Tag(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITERMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除')
+    )
+    name = models.CharField(max_length=10, verbose_name="名称")
+    status = models.PositiveBigIntegerField(default=STATUS_NORMAL, choices=STATUS_ITERMS, verbose_name="状态")
+    owner = models.ForeignKey(User, verbose_name="作者")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = "分类"
