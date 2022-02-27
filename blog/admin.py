99ce557de479edb433609a173019b8a0c890eda6
juliusjli <juliusjli@tencent.com>
2022-02-27 08:46:58 +0800

    add some admin code

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Category,Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','status','is_nav','owner','create_time')
+    fields = ('name','status','is_nav','owner')
+
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','status','owner','create_time')
+    fields = ('name','status','owner')
--- typeidea/settings.py
+++ typeidea/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'typeidea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '9.134.39.165',
+        'PORT': 3306
     }
 }
 
